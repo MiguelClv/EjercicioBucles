863036666a3548bd3a52c8357637b3f5f496dabc
MiguelClv <lvp.acount@gmail.com>
2016-10-14 20:01:17 +0200

    Cuarto ejercicio

---
+++ src/com/company/EX04.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by 54167208T on 14/10/2016.
+ */
+public class EX04 {
+    public static void main(String[] args) {
+        for(int i = 0;i<=100;i++){
+            System.out.println("MiguelClv");
+        }
+    }
+    }
