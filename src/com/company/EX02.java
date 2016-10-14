d361856ec23b692e06459eb72d3665a33c580942
MiguelClv <lvp.acount@gmail.com>
2016-10-14 19:54:07 +0200

    Segundo y tercer ejercicio

---
+++ src/com/company/EX02.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by 54167208T on 14/10/2016.
+ */
+public class EX02 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        for(int i=5;i<=500;i++)
+            System.out.println(i);
+    }
+}
---
+++ src/com/company/EX03.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by 54167208T on 14/10/2016.
+ */
+public class EX03 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        for(int i=0;i<=10000;i++){
+            if(i%5==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
