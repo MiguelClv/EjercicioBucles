81dc1f1c66c95d1cb8e37e7dc90a37be101dfadc
MiguelClv <lvp.acount@gmail.com>
2016-10-14 20:13:29 +0200

    Quinto ejercicio

---
+++ src/com/company/EX06.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by 54167208T on 14/10/2016.
+ */
+public class EX06 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        for(int i = 0;i<=1000;i++){
+            if(i%2==0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
