e9e16252d1552d31cf4a6d327e321c572d8fdca7
MiguelClv <lvp.acount@gmail.com>
2016-10-14 19:52:07 +0200

    Primer ejercicio

---
+++ src/com/company/EX01.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by 54167208T on 14/10/2016.
+ */
+public class EX01 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        for(int i = 0;i<=1000;i++){
+            System.out.println(i);
+        }
+    }
+}
